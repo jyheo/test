d8d63283408b6ab52ac0a5b1e16f9369a7321814
LeeInHaeng <lih0420@naver.com>
2016-11-25 15:14:51 +0900

    변수 j추가 , 범위 1~9로 변경
    5,6,7라인 변경

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-  int i;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
+  int i,j;
+  for (i = 1; i < 9; i++) {
+    for (j = 1; j < 9; j++) {
       printf("%dx%d=%d ", i, j, i * j);
     }
   }
-  
+  printf("my gugudan!\n");
   return 0;
 }
