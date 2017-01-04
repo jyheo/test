bd8e88ca39440e53b344e9c4bb99ef2959ea9a4a
Moon <mys9005@kpu.ac.kr>
2017-01-04 15:49:45 +0900

    test-1

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-  int i;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
-    }
-  }
-  
-  return 0;
+	int i;
+	int j = 1;
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 9; j++) {
+			printf("%dx%d=%d ", i, j, i * j);
+		}
+	}
+
+	return 0;
 }
 
 // comment
