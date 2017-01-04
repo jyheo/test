36def4b028bd3848a7e3b0a6f6c67b5061c3e6e2
Myung soo <vksxkwl65@gmail.com>
2017-01-04 15:27:41 +0900

    add j and change 0->1

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-  int i;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
+  int i,j;
+  for (i = 1; i < 5; i++) {
+    for (j = 1; j < 9; j++) {
       printf("%dx%d=%d ", i, j, i * j);
     }
   }
