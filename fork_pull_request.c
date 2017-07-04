381df1b45ac0b0d081340e236f06faf0a17b1efe
even1415 <even1415@gmail.com>
2017-07-04 14:43:35 +0900

    even1415's pull

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -3,10 +3,13 @@
 int main()
 {
   int i;
+  int j; //not declared
+
   for (i = 0; i < 5; i++) {
     for (j = 0; j < 9; j++) {
       printf("%dx%d=%d ", i, j, i * j);
     }
+	printf("\n"); //line up to 1~5
   }
   
   return 0;
