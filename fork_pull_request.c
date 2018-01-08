5d24685250444cc62d06dc458017995f32ef5733
양민규 <even1415@gmail.com>
2018-01-08 13:54:55 +0900

    even1415's commit

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -5,9 +5,9 @@ int main()
   int i;
   int j; //not declared
 
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+  for (i = 0; i <= 5; i++) {
+    for (j = 0; j <= 9; j++) {
+      printf("%dx%d=%d \t", i, j, i * j);
     }
 	printf("\n"); //line up to 1~5
   }
@@ -16,3 +16,6 @@ int main()
 }
 
 // comment
+//before [i<4] -> after [i<=5]
+//before [j<9] -> after [j<=9]
+//add "\t"
