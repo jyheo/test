72ae6fa725984d662e31c1a8465ca921aa1382c9
JeonMinJae <38930501+JeonMinJae@users.noreply.github.com>
2018-06-29 14:54:04 +0900

    Update fork_pull_request.c

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -3,13 +3,13 @@
 int main()
 {
   int i,j;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
+  for (i = 1; i < 9; i++) {
+    for (j = 1; j < 9; j++) {
       printf("%dx%d=%d ", i, j, i * j);
     }
   }
   
-  //return 0;
+  return 0;
 }
 
 // comment
