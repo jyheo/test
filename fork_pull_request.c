7f91e59e4cff0b8da58c6748baf6d50a8027b944
yelim <vigvang_bip@naver.com>
2018-06-29 15:59:44 +0900

    ddddd

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
   int i,j;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
+  for (i = 1; i < 10; i++) {
+    for (j = 1; j<10; j++) {
       printf("%dx%d=%d ", i, j, i * j);
     }
   }
@@ -12,4 +12,7 @@ int main()
   return 0;
 }
 
-// comment
+// comment 
+// 뭐지 뭘까 뭔지 1도 모르게따
+//
+// 
