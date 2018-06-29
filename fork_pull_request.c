59c9dc804e41dd2ee9f4e4cce1e88cb6f71fa8f3
Woo JeongYeon <woo9515jy@gmail.com>
2018-06-29 15:07:19 +0900

    modify gugudan

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -3,9 +3,9 @@
 int main()
 {
   int i,j;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+  for (i = 1; i <= 5; i++) {
+    for (j = 1; j <= 9; j++) {
+      printf("%dx%d=%d\n", i, j, i * j);
     }
   }
   
