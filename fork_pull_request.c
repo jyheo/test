5a79e00230bb746bf5771b2849c85b04e029893e
tae <tyt@naver.com>
2018-06-29 15:55:40 +0900

    last

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -2,14 +2,7 @@ include <stdio.h>
 
 int main()
 {
-  int i,j;
-  for (i = 0; i < 31; i++) {
-    for (j = 0; j < 9; j++) {
-      print( j, i * j);
-    }
-  }
-  
-  return 0;
-}
+
+   :}
 
 // comment
