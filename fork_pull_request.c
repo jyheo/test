d4604ff1e91ee58463a6d3150c67c453c8d07d49
Kwonkyu <park2348190@naver.com>
2018-06-29 00:38:44 -0700

    added intro

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -3,13 +3,14 @@
 int main()
 {
   int i,j;
+	printf("============================ GUGUDAN STARTED ==========================\n");
   for (i = 1; i <= 9; i++) {
     for (j = 1; j <= 9; j++) {
       printf("%dx%d=%2d ", j, i, i * j);
     }
 	printf("\n");
   }
-	printf("GUGUDAN ENDED.");  
+	printf("============================ GUGUDAN ENDED. ==========================\n");  
   return 0;
 }
 
