4ae15ffb52367a742476f883d22f7b1bf8609d40
pyeongki Kim <aorca603@naver.com>
2018-12-28 16:08:17 +0900

    fork_pull_request.c modified

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -5,11 +5,11 @@ int main()
   int i,j;
   for (i = 0; i < 5; i++) {
     for (j = 0; j < 9; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+      printf("%d x %d = %d ", i, j, (i*j));
     }
   }
   
   return 0;
 }
 
-// comment
+// 김평기 수정
