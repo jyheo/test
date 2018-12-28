a88a0edbf91c54d449c77b47070963bfd51fb8e7
son <thsehdrl1@kpu.ac.kr>
2018-12-28 15:58:01 +0900

    add fork_pull_request.c

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -3,8 +3,10 @@
 int main()
 {
   int i,j;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
+  for (i = 0; i < 5; i++)
+  {
+    for (j = 0; j < 9; j++) 
+    {
       printf("%dx%d=%d ", i, j, i * j);
     }
   }
