561a77775458296e77df1767cf982abf0beade1d
KimRyunHyeon <winxp4333@naver.com>
2018-12-28 16:02:34 +0900

    change fork_pull_request.c

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -5,10 +5,10 @@ int main()
   int i,j;
   for (i = 0; i < 5; i++) {
     for (j = 0; j < 9; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+      printf("%d x %d = %d ", i, j, i * j);
     }
   }
-  
+
   return 0;
 }
 
