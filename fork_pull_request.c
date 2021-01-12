abf755aaf881cb29ad829707b401cd680182c6c5
Inho Choi <born7sh@gmail.com>
2021-01-12 15:47:37 +0900

    modified fork_pull_request.c

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -5,8 +5,9 @@ int main()
   int i,j;
   for (i = 2; i < 10; i++) {
     for (j = 1; j < 10; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+      printf("%dx%d=%d\t", i, j, i * j);
     }
+    printf("\n");
   }
   
   return 0;
