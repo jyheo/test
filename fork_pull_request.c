da7b9ca41197a72ec82a3e1f5f5905304db41565
Hyun Su <jwzz9324@kpu.ac.kr>
2021-07-28 17:17:52 +0900

    request modified

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -2,14 +2,16 @@
 
 int main()
 {
-  int i,j;
-  for (i = 2; i < 10; i++) {
-    for (j = 1; j < 10; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+    int i, j;
+    for (i = 2; i < 10; i++) {
+        printf("===%d단===\n",i);
+        for (j = 1; j < 10; j++) {
+            printf("%d x %d=%d\n", i, j, i * j);
+        }
+        printf("\n\n");
     }
-  }
-  
-  return 0;
+
+    return 0;
 }
 
 // comment
