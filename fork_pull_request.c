a3e72d0a26ba518c4a13702af74e62fc09212f98
Jisu Jang <wkdwltn1015@naver.com>
2022-07-07 14:18:56 +0900

    2

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -2,14 +2,13 @@
 
 int main()
 {
-  int i,j;
-  for (i = 2; i < 10; i++) {
-    for (j = 1; j < 10; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
-    }
-  }
-  
-  return 0;
-}
+	int i, j;
+	for (i = 2; i < 10; i++) {
+		for (j = 1; j < 10; j++) {
+			printf("%dx%d=%d ", i, j, i * j);
+		}
+		printf("\n");
+	}
 
-// comment
+	return 0;
+}
