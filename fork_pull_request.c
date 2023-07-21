45dbdd53a8a7eefbc212e0dfd24ff8a37ad9cc49
entellaKa <inchol321@gmail.com>
2023-07-21 13:47:00 +0900

    modify fork_pull_request.c

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -5,7 +5,7 @@ int main()
   int i,j;
   for (i = 2; i < 10; i++) {
     for (j = 1; j < 10; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+      printf("%dx%d=%2d\n", i, j, i * j);
     }
   }
   
